63c8b89c49792ba823074f125237e7788fce4035
pks2906 <pratik1971sinha@gmail.com>
2024-07-23 22:19:51 +0530

    changes

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import { ClientSideSuspense, RoomProvider } from '@liveblocks/react/suspense'
 
@@ -6,16 +6,35 @@ import Header from '@/components/Header'
 import { Editor } from '@/components/ui/editor/Editor'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import ActiveCollaborators from './ActiveCollaborators'
+import {  useRef, useState } from 'react';
+import { Input } from './ui/input'
 
 
-const CollaborativeRoom = ({roomId, roomMetadata}: CollaborativeRoomProps) => {
+const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType }: CollaborativeRoomProps) => {
+  const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
+  const [editing, setEditing] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLDivElement>(null);
+  
   return (
     <RoomProvider id={roomId}>
         <ClientSideSuspense fallback={<div>Loading…</div>}>
-          <div className="collaborative-room">
+          <div className="collaborative-room">  
           <Header>
-             <div className="flex w-fit items-center justify-center gap-2">
-              <p className="document-title">Share</p>
+             <div ref={containerRef} className="flex w-fit items-center justify-center gap-2">
+              {editing && !loading ? (
+                <Input
+                
+                />
+ 
+              ) : (
+                <>
+                  <p className="document-title">{documentTitle}</p>
+                </>
+              )}
+
 
         </div>
         <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
