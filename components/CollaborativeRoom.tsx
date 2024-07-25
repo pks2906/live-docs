1b2447b51cfb3857fb342bf0cc316c245942a3ca
pks2906 <pratik1971sinha@gmail.com>
2024-07-25 20:50:14 +0530

    changes

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -6,9 +6,12 @@ import Header from '@/components/Header'
 import { Editor } from '@/components/ui/editor/Editor'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import ActiveCollaborators from './ActiveCollaborators'
-import {  useRef, useState } from 'react';
+import {  useEffect, useRef, useState } from 'react';
 import { Input } from './ui/input'
 import Image from 'next/image';
+import { updateDocument } from '@/lib/actions/room.actions';
+
+
 
 
 const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType }: CollaborativeRoomProps) => {
@@ -19,9 +22,52 @@ const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType }: Col
   const containerRef = useRef<HTMLDivElement>(null);
   const inputRef = useRef<HTMLDivElement>(null);
 
-  const updateTitleHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+  const updateTitleHandler = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if(e.key === 'Enter') {
+      setLoading(true);
+
+      try {
+        if(documentTitle !== roomMetadata.title) {
+          const updatedDocument = await updateDocument(roomId, documentTitle);
+
+          if(updatedDocument) {
+            setEditing(false);
+          } 
+        }
+        
+      } catch (error) {
+        console.error(error); 
+      }
+
+      setLoading(false);
+    }
 
   }
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if(containerRef.current && !containerRef.current.contains(e.target as Node)) {
+        setEditing(false);
+        updateDocument(roomId, documentTitle);
+      } 
+    }
+
+
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+  }, [roomId,documentTitle])
+
+  useEffect(() => {
+    if(editing && inputRef.current){
+      inputRef.current.focus();
+    }
+
+  }, [editing])
   
   return (
     <RoomProvider id={roomId}>
--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -56,3 +56,19 @@ export const getDocument = async ({ roomId, userId}: { roomId: string; userId: s
   }
 
 }
+
+export const updateDocument = async(roomId: string, title: string) => {
+  try {
+    const updatedRoom = await liveblocks.updateRoom(roomId, {
+      metadata: {
+        title
+      }
+    })
+    revalidatePath('/documents/${roomId}');
+    return parseStringify(updatedRoom);
+    
+  } catch (error) {
+    console.log('Error happened while updating a room: ${error}');
+    
+  }
+}
